e78a3e66ae3153ae42e48ad81ef44729f4d10797
zer0day98 <zer0day98@hotmail.com>
2021-10-20 17:36:29 +0300

    Create subChecker.py

---
+++ subChecker.py
@@ -0,0 +1,32 @@
+import optparse
+
+import requests
+
+
+def get_user_input():
+    parse_object = optparse.OptionParser()
+    parse_object.add_option("-i", "--input", dest="input", help="input for file")
+    return parse_object.parse_args()
+
+
+def subcheck(input):
+    file1 = open(input, 'r')
+    Lines = file1.readlines()
+    count = 0
+    count2 = 0
+    for line in Lines:
+        try:
+            a: str = ('https://{0}'.format(line.strip()))
+            r = requests.get(a)
+
+            if r.status_code != 404:
+                print(a)
+        except:
+            count2 += 1
+    print("Reachable websites count are: " + count)
+    print("Unreachable websites count are: " + count2)
+
+
+print("Sub Checker Started!! ")
+(user_input, arguments) = get_user_input()
+subcheck(user_input.input)
